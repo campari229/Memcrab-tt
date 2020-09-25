2aecf8472d903e1536f6161f59428a07efcc890f
Campari229 <KennyKoin@gmail.com>
2020-09-25 17:47:22 +0300

    Updates

---
+++ precache-manifest.31c22702c76cb74d419d68dd94f754d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c047016acfb85ea331f6961c4f3a8890",
+    "url": "/Memcrab-tt/index.html"
+  },
+  {
+    "revision": "0bb2eeb137fe5c8efe71",
+    "url": "/Memcrab-tt/static/css/main.d3f1bc8a.chunk.css"
+  },
+  {
+    "revision": "b833c98b1b4f450cb682",
+    "url": "/Memcrab-tt/static/js/2.9d077a1e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Memcrab-tt/static/js/2.9d077a1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bb2eeb137fe5c8efe71",
+    "url": "/Memcrab-tt/static/js/main.7f00a4bb.chunk.js"
+  },
+  {
+    "revision": "49a9df0a26d782604222",
+    "url": "/Memcrab-tt/static/js/runtime-main.df2c4a34.js"
+  }
+]);
